b214dea00a17f17e053ee313400b158ec7bc6026
ahmadramadhan <ahmadoneperson@gmail.com>
2024-08-17 16:42:01 +0700

    chore: initial setup alpinejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,17 @@ import react from "@astrojs/react";
 import preact from "@astrojs/preact";
 import svelte from "@astrojs/svelte";
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    solidJs({
-      include: ['**/solid/*', '**/node_modules/@suid/material/**']
-    }),
-    react({
-      include: ["**/react/*"]
-    }),
-    preact({
-      compat: true,
-      include: ["**/preact/*"]
-    }),
-    svelte(),
-  ],
+  integrations: [solidJs({
+    include: ['**/solid/*', '**/node_modules/@suid/material/**']
+  }), react({
+    include: ["**/react/*"]
+  }), preact({
+    compat: true,
+    include: ["**/preact/*"]
+  }), svelte(), alpinejs()],
   output: "server"
 });
